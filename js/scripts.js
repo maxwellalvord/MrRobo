6ebfd29e5f3bff5b8d34e7d8d58d8a5a18ed4aa7
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-10 09:54:59 -0700

    WIP: to fix logic in test one

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
 function beepBoop(number) {
-  let finalArray = [];
+  // let finalArray = [];
   let numberArray = Array.from(String(number), Number) 
-  console.log(numberArray);
-  numberArray.forEach (function(number) {
-      finalArray.push(numberArray - (number));
-      finalArray.push(numberArray - (number-1));
-      finalArray.push(numberArray - (number-2));
-      finalArray.push(numberArray - (number-3));
-      finalArray.push(numberArray - (number-4));
-      finalArray.push(number);
-      console.log(finalArray);
-    
-  }); return finalArray;
+  const finalArray = numberArray.map(function(number) {
+    number.range(0, number)
+  });
 }
+    
+  
+  
 
 
-
+// Business logic
+// function beepBoop(number) {
+//   let finalArray = [];
+//   let numberArray = Array.from(String(number), Number) 
+//   console.log(numberArray);
+//   numberArray.forEach (function(number) {
+//     console.log(number)
+//       finalArray.push(numberArray - (number));
+//       finalArray.push(numberArray - (number-1));
+//       finalArray.push(numberArray - (number-2));
+//       finalArray.push(numberArray - (number-3));
+//       finalArray.push(numberArray - (number-4));
+//       finalArray.push(numberArray - (number-5));
+//       finalArray.push(numberArray - (number-6));
+//       finalArray.push(numberArray - (number-7));
+//       finalArray.push(number);
+//       console.log(finalArray);
+    
+//   }); return finalArray;
+// }
 
 
 
